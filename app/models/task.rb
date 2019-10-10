9e1c7fdab5289378576714ab278f8a4fd26abce7
s-peace <ssmim777@gmail.com>
2019-10-10 16:02:00 +0900

    done validation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,10 @@
 class Task < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 30}
+  validate :validate_name_noto_including_comma
+
+  private
+  
+    def validate_name_noto_including_comma
+      errors.add(:name, 'にカンマをふくめる事は出来ません。') if name&.include?(',')
+    end
 end
