07e3aeed3e2debf514c0b171a053a8be8630af3a
s-peace <ssmim777@gmail.com>
2019-10-15 21:56:45 +0900

    amend test

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -2,21 +2,23 @@ require 'rails_helper'
 
 describe 'タスク管理機能', type: :system do
   describe '一覧表示機能' do
+    # ユーザーＡを作成しておく
+    let(:user_a) {FactoryBot.create(:user, name: 'ユーザーＡ', email: 'a@example.com')}
+    # ユーザーＢを作成しておく
+    let(:user_b) {FactoryBot.create(:user, name: 'ユーザーＢ', email: 'b@example.com')}
+
     before do
-      # ユーザーＡを作成しておく
-      user_a = FactoryBot.create(:user, name: 'ユーザーＡ', email: 'a@example.com')
       # 作成者がユーザーＡであるタスクを作成しておく
       FactoryBot.create(:task, name: '最初のタスク', user: user_a)
+
+      visit login_path
+      fill_in 'メールアドレス', with: login_user.email
+      fill_in 'パスワード', with: login_user.password
+      click_button 'ログインする'
     end
   
     context 'ユーザーＡがログインしているとき' do
-      before do
-        # ユーザーＡでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'a@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button 'ログインする'
-      end
+      let(:login_user) {user_a}
 
       it 'ユーザーＡが作成したタスクが表示される' do
         # 作成済みのタスクの名称が画面上に表示されていることを確認
@@ -25,15 +27,7 @@ describe 'タスク管理機能', type: :system do
     end
 
     context 'ユーザーＢがログインしているとき' do
-      before do
-        # ユーザーＢを作成しておく
-        FactoryBot.create(:user, name: 'ユーザーＢ', email: 'b@example.com')
-        # ユーザーＢでログインする
-        visit login_path
-        fill_in 'メールアドレス', with: 'b@example.com'
-        fill_in 'パスワード', with: 'password'
-        click_button
-      end
+      let(:login_user) {user_b}
 
       it 'ユーザーＡが作成したタスクが表示されない' do
         # ユーザーＡが作成したタスクの名称が画面上に表示されていない事を確認
